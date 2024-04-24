b56597facebc5e18f06c89832739ae0206850381
Samuel-Monteiro <samuelsilva91935@gmail.com>
2024-04-24 18:36:24 -0300

    Inicio do js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("Teste");
+const busca = async () => {
+    const url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?type=spell%20card&language=pt";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+const main = async () => {
+    const dados = await busca();
+    dados.array.forEach(element => {
+        iterar(element)
+    });
+}
+
+const iterar = (element) => {
+    console.log(element)
+}
+
+main()
